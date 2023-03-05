1255e6c5a3e1a51bed1d37c1d000829a330f156e
SalilBajracharya <salil.bajracharya911@gmail.com>
2023-03-05 16:09:14 +0545

    Added Automapper and Dtos
    * Added DTOs according to the domain entites.
    * Added Automapper Package and Mapping profiles for mapping DTOs and Domain Entities.
    * Created Mapping Profile for Automapper.
    * Created ApplicationServiceRegistration class for dependency injection and registered automapper.

---
+++ Hr.LeaveManagement.Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,18 @@
+﻿using Hr.LeaveManagement.Application.DTOs.Common;
+using Hr.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.DTOs
+{
+    public class LeaveAllocationDto : BaseDto
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
---
+++ Hr.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Hr.LeaveManagement.Application.DTOs;
+using Hr.LeaveManagement.Application.DTOs.LeaveRequest;
+using Hr.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
