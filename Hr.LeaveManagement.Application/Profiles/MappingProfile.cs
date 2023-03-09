1c038026410e38a18b762b7a2d0847dbe8ee3b4b
SalilBajracharya <salil.bajracharya911@gmail.com>
2023-03-09 12:27:00 +0545

    Minor fixes in Application and Domain projects * Created Mappings for all the DTOs respectively.

--- Hr.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ Hr.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -2,6 +2,7 @@
 using Hr.LeaveManagement.Application.DTOs;
 using Hr.LeaveManagement.Application.DTOs.LeaveAllocations;
 using Hr.LeaveManagement.Application.DTOs.LeaveRequests;
+using Hr.LeaveManagement.Application.DTOs.LeaveType;
 using Hr.LeaveManagement.Domain;
 
 namespace Hr.LeaveManagement.Application.Profiles
@@ -10,10 +11,24 @@ namespace Hr.LeaveManagement.Application.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveRequest Mappings
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion
+
+            #region LeaveAllocation Mappings
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
+
+            #region Leavetype Mappings
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, UpdateLeaveTypeDto>().ReverseMap();
+            #endregion
         }
     }
 }
