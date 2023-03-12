de1c2ea4ecfef1be507737e9d042b5a26a0f0e6d
SalilBajracharya <salil.bajracharya911@gmail.com>
2023-03-12 16:14:41 +0545

    Setting up infrastructure project * Created IEmailSender in APplication layer and created implementation of it on Infrastructure Layer. * Created Models related to Email * Created ServiceRegistrar for Infrastructure Layer for registering services.

--- Hr.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ Hr.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Hr.LeaveManagement.Application.Contracts.Infrastructure;
 using Hr.LeaveManagement.Application.Contracts.Persistence;
 using Hr.LeaveManagement.Application.DTOs.LeaveRequests.Validators;
 using Hr.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using Hr.LeaveManagement.Application.Model;
 using Hr.LeaveManagement.Application.Responses;
 using Hr.LeaveManagement.Domain;
 using MediatR;
@@ -15,12 +17,14 @@ namespace Hr.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
         public readonly ILeaveRequestRepository _leaveRequestRepository;
         public readonly IMapper _mapper;
         public readonly ILeaveTypeRepository _leaveTypeRepository;
+        public readonly IEmailSender _emailSender;
 
-        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, ILeaveTypeRepository leaveTypeRepository, IEmailSender emailSender)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
             _leaveTypeRepository = leaveTypeRepository;
+            _emailSender = emailSender;
         }
 
         public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
@@ -36,7 +40,7 @@ namespace Hr.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
                 response.Errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
                 //throw new ValidationException(validationResult);
             }
-            
+
 
             var leaveRequest = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
 
@@ -45,6 +49,20 @@ namespace Hr.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
             response.Success = true;
             response.Message = "Creation Successful";
             response.Id = leaveRequest.Id;
+
+            var email = new Email
+            {
+                To = "employee@org.com",
+                Body = $"Your leave request for {request.LeaveRequestDto.StartDate:D} to {request.LeaveRequestDto.EndDate:D} has been" +
+                $"submitted successfully.",
+                Subject = "Leave Request Submitted."
+            };
+            try {
+                await _emailSender.SendEmail(email);
+            } catch (Exception ex)
+            {
+
+            }
 
             return response;
         }
---
+++ Hr.LeaveManagement.Infrastructure/Registrar/InfrastructureServicesRegistration.cs
@@ -0,0 +1,26 @@
+﻿using EllipticCurve;
+using Hr.LeaveManagement.Application.Contracts.Infrastructure;
+using Hr.LeaveManagement.Application.Model;
+using Hr.LeaveManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Infrastructure.Registrar
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigurationInfrastrutureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            return services;
+        }
+    }
+}
